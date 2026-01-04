faba76509dcdf8a088a639954977f8917233cb7b
Mario T. Lanza <me@mlanza.com>
2026-01-03 20:50:50 -0500

    save

--- bin/nt.d/note.js
+++ bin/nt.d/note.js
@@ -544,8 +544,12 @@ function path(){
 function tskNamed(id){
   return new Task(async function(reject, resolve){
     try {
-      const {normalized} = await identify(id);
-      resolve(normalized);
+      if (id) {
+        const {normalized} = await identify(id);
+        resolve(normalized);
+      } else {
+        resolve(null);
+      }
     } catch (error) {
       reject(error);
     }
@@ -742,10 +746,10 @@ function props(options){
   const headingLevel = options.heading === 0 ? 0 : Math.max(1, Math.min(5, parseInt(options.heading) || 1));
   const format = options.json ? 'json' : options.format || "md";
   return function(pageName, propName = null){
-    return normal(pageName).
+    return pageName ? normal(pageName).
       chain(Task.juxt(Task.of, qryPage)).
       map(fmtProps({format, heading: headingLevel > 0}, propName)).
-      map(fmtBody({format, heading: headingLevel}));
+      map(fmtBody({format, heading: headingLevel})) : Task.of(null);
   }
 }
 
