ee67379717d791b1ac9695e578888a7823e557fe
Mario T. Lanza <me@mlanza.com>
2026-01-08 20:39:56 -0500

    save

--- bin/nt.d/blocked.ps1
+++ bin/nt.d/blocked.ps1
@@ -1,13 +1,22 @@
 #!/usr/bin/env pwsh
 
 # blocked.ps1 - Processes stdin, prepending "- " to lines that don't start with a space
+# Only transforms content if no lines start with " " or "- "
 # Usage: Get-Content input.txt | ./blocked.ps1
 
+# Read all input lines first
+$lines = @()
 while ($input.MoveNext()) {
-    $line = $input.Current
-    if ($line.Length -gt 0 -and $line[0] -ne ' ' -and $line[0] -ne '-') {
-        "- " + $line
-    } else {
-        $line
-    }
+    $lines += $input.Current
+}
+
+# Check if any line starts with space or "- "
+$hasBlockedLines = $lines | Where-Object { $_.StartsWith(" ") -or $_.StartsWith("- ") } | Measure-Object | Select-Object -ExpandProperty Count
+
+if ($hasBlockedLines -gt 0) {
+    # Output original content unchanged
+    $lines
+} else {
+    # Prepend "- " to each line
+    $lines | ForEach-Object { "- " + $_ }
 }
