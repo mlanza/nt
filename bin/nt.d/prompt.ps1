5484a16f0b6d2d448b8b823ecdafaca58ee418a4
Mario T. Lanza <me@mlanza.com>
2026-01-13 08:19:08 -0500

    save

--- bin/nt.d/prompt.ps1
+++ bin/nt.d/prompt.ps1
@@ -5,12 +5,7 @@ $wikilinks = $prompt | nt commented | nt wikilinks | nt prereq
 
 $prompt
 if ($LASTEXITCODE -eq 0) {
-  if ($wikilinks) {
-    $copy = $wikilinks | nt seen | nt page --less --heading=2 | nt tidy
-    $copy | nt sep
-    $others = $copy | nt wikilinks | nt seen
-    $others | Where-Object { $_ -notin ($wikilinks) } | sort | nt props tags description -r description -u description --heading=2 | nt sep
-  }
+  $wikilinks | nt about
 } else {
   $code = $LASTEXITCODE
   Write-Host "⚠️ Wikilink expansion is not enabled."
