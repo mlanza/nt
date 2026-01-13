53251b858f053d1673757bd0983a7ee0fb9704fc
Mario T. Lanza <me@mlanza.com>
2026-01-13 13:52:30 -0500

    save

--- bin/nt.d/about.ps1
+++ bin/nt.d/about.ps1
@@ -2,5 +2,5 @@
 $topics = @($input) + @($args)
 $body = $topics | nt seen | nt prereq | nt seen | nt page --less | nt tidy
 $body
-$mentioned = $body | nt wikilinks | nt seen
+$mentioned = $body | nt wikilinks | nt seen | nt n --unaliased
 $mentioned | Where-Object { $_ -notin ($topics) } | sort | nt props tags situation -r situation --heading=2 | nt sep "# See Also"
