d0882339eed9fa6884ddbef6d3f1c054bdd9a4c5
M. Lanza <me@mlanza.com>
2026-05-30 13:04:32 -0400

    latest stable release

--- bin/nt.d/sep.ps1
+++ bin/nt.d/sep.ps1
@@ -1,7 +1,10 @@
 #!/usr/bin/env pwsh
 param(
-  [string]$Separator = '---'
+  [string]$Separator = '\n---'
 )
+
+# Support standard escape sequences (e.g. \n, \r, \t) in the separator
+$Separator = [regex]::Unescape($Separator)
 
 $seen = $false
 
